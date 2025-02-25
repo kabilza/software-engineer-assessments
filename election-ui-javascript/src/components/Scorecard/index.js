52bb13a162f433c4ace11fbcfaa6399ca3a687f7
Pawaris Wongsalung <48781587+kabilza@users.noreply.github.com>
2025-02-25 21:44:51 +0700

    Added yellow background to winner
    Added yellow background to winner

--- election-ui-javascript/src/components/Scorecard/index.js
+++ election-ui-javascript/src/components/Scorecard/index.js
@@ -8,7 +8,7 @@ function Scorecard({ results }) {
   let scores = [];
   for (let i=0; i < results.length; i++) {
     scores.push(
-      <tr key={i}>
+      <tr className={results[i].winner ? 'winner' : ''} key={i}>
         <td>{results[i].party}</td>
         <td>{results[i].name}</td>
         <td>{results[i].votes}</td>
