90d64a850d1a971d7ecc2656d5204720f276f6df
Pawaris Wongsalung <48781587+kabilza@users.noreply.github.com>
2025-02-25 07:02:23 +0700

    Update Party Links Button CSS

--- election-ui-javascript/src/components/PartyLinks/index.js
+++ election-ui-javascript/src/components/PartyLinks/index.js
@@ -1,23 +1,29 @@
 import './PartyLinks.css';
 
 const PARTIES = [
-  { name: 'Hippo Party', url: 'https://en.wikipedia.org/wiki/Hippopotamus'},
-  { name: 'Giraffe Party', url: 'https://en.wikipedia.org/wiki/Giraffe'},
-  { name: 'Tiger Party', url: 'https://en.wikipedia.org/wiki/Tiger'},
-  { name: 'Zebra Party', url: 'https://en.wikipedia.org/wiki/Zebra'},
-  { name: 'Meerkat Party', url: 'https://en.wikipedia.org/wiki/Meerkat'},
-  { name: 'Pelican Party', url: 'https://en.wikipedia.org/wiki/Pelican'},
+  { name: 'Hippo Party', url: 'https://en.wikipedia.org/wiki/Hippopotamus' },
+  { name: 'Giraffe Party', url: 'https://en.wikipedia.org/wiki/Giraffe' },
+  { name: 'Tiger Party', url: 'https://en.wikipedia.org/wiki/Tiger' },
+  { name: 'Zebra Party', url: 'https://en.wikipedia.org/wiki/Zebra' },
+  { name: 'Meerkat Party', url: 'https://en.wikipedia.org/wiki/Meerkat' },
+  { name: 'Pelican Party', url: 'https://en.wikipedia.org/wiki/Pelican' },
 ]
 
 function PartyLinks() {
   return (
-    <ul className="Party-links">
+    // <ul className="Party-links">
+    //   {PARTIES.map((party) =>
+    //     (<li className="Party-link-button" key={party.name}>
+    //       <a href={party.url}>{party.name}</a>
+    //     </li>)
+    //   )}
+    // </ul>
+    <div className='Party-links-container'>
       {PARTIES.map((party) =>
-        (<li className="Party-link" key={party.name}>
-          <a href={party.url}>{party.name}</a>
-        </li>)
-      )}
-    </ul>
+      (<a className="Party-links-button" href={party.url} key={party.name}>
+        {party.name}
+      </a>))}
+    </div>
   );
 }
 
